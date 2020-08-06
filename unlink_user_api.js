6f8aac868f58c9b4b3ddf62d1e2699e836353637
Azario16 <61908140+Azario16@users.noreply.github.com>
2020-08-06 16:57:44 +0500

    Update unlink_user_api.js

--- unlink_user_api.js
+++ unlink_user_api.js
@@ -1,17 +1,18 @@
+//Рабочая версия 
 // Получаем Id П 
 fetch("https://rooms.vimbox.skyeng.ru/users/api/v1/auth/config", 
-{"method": "POST"})
+{"credentials":"include","method": "POST"})
 .then(response => { if (response.ok === true) { return response.json(); } else { return null; } })
 .then(json => {teacher_id=(json.user.id)
       //Получаем ID учеников
-      fetch("https://rooms.vimbox.skyeng.ru/users/api/v1/teachers/"+teacher_id+"/students", {})
+      fetch("https://rooms.vimbox.skyeng.ru/users/api/v1/teachers/"+teacher_id+"/students", {"credentials":"include", })
       .then(response => { if (response.ok === true) { return response.json(); } else { return null; } })
       .then(json => {a=(json.length) 
                         for (var i = 0; i < a; i++){
                           userid = ((json)[i].id)
                           console.log(userid)
                           //Запрос на удаление
-                          fetch("https://rooms.vimbox.skyeng.ru/users/api/v1/teachers/unlink-student/"+ userid +"", {"method": "POST",});
+                          fetch("https://rooms.vimbox.skyeng.ru/users/api/v1/teachers/unlink-student/"+ userid +"", {"credentials":"include", "method": "POST",});
                         }  
         })
 })
